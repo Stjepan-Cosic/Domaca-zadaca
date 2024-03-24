8c037ff32d981f66a86e766c857e9192caee545c
Stjepan <161949509+Stjepan-Cosic@users.noreply.github.com>
2024-03-24 19:49:15 +0100

    Update DomacaZadaca4_publisher.py

--- DomacaZadaca4_publisher.py
+++ DomacaZadaca4_publisher.py
@@ -6,7 +6,7 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 1 # sekunde
+        timer_period = 1 
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
@@ -21,9 +21,7 @@ def main(args=None):
     rclpy.init(args=args)
     minimal_publisher = MinimalPublisher()
     rclpy.spin(minimal_publisher)
-    # Eksplicitno uništavanje čvora
-    # (opciono - inače će se automatski uništiti
-    # kada sakupljač smeća uništi objekat čvora)
+ 
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
